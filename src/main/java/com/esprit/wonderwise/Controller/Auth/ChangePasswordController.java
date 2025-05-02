5bb85b7dcd09ec9e0a0aafc9a55b6533f9255742
Rayen Aouini <aouinirayen44@gmail.com>
2025-05-02 13:53:09 +0100

    resolution

--- src/main/java/com/esprit/wonderwise/Controller/Auth/ChangePasswordController.java
+++ src/main/java/com/esprit/wonderwise/Controller/Auth/ChangePasswordController.java
@@ -3,23 +3,10 @@ package com.esprit.wonderwise.Controller.Auth;
 import com.esprit.wonderwise.Service.UserService;
 import com.esprit.wonderwise.Utils.DialogUtils;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.scene.image.ImageView;
+import .ImageView;
 import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Control;
 import java.io.IOException;
 
-public class ChangePasswordController {
-
-
-
-
-
-
-
-}
