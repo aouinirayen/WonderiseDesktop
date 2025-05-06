75ae8ef75e538deeda4ac65762d34ae5efc01bdc
Fatma ezzahra charfi <Fatmaezzahra.Charfi@esprit.tn>
2025-05-06 14:14:54 +0100

    first

--- src/main/java/com/esprit/wonderwise/MainFx.java
+++ src/main/java/com/esprit/wonderwise/MainFx.java
@@ -4,7 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-aaaaaaaaaaaa
+
 import java.io.IOException;
 
 public class MainFx extends Application {
